f2b88aa8632bdf63a361f1b004ba5faef6a72a97
Honungspil <niklasaasberg@gmail.com>
2019-05-24 15:06:00 +0200

    15:05 upd

--- login-process.php
+++ login-process.php
@@ -1,1 +1,22 @@
-
+<?php
+include_once("assets\bootstrap.php");
+#Hämtar datan från formuläret
+$email = $_POST['email'];
+$pw = $_POST['password'];
+#Validerar formdatan och kontrollerar lösenorder om validering går igenom
+if(val_log($email, $pw)){
+    if(md5(get_one($email, "salt") . $pw) === get_one($email, "pw"))
+    {
+      $_SESSION['email'] = $email;
+      $_SESSION['loggedin'] = true;
+      header("Location: index.php?msg=Log in successful");
+    }
+    else{
+      header("Location: login.php?msg=Incorrect email or password.");
+      break;
+    }
+  }
+  else {
+    header("Location: login.php?msg=Incorrect email or password");
+    break;
+  }
--- views/_login.php
+++ views/_login.php
@@ -3,14 +3,14 @@
   <head>
   </head>
   <div class="login">
-  <h1>Vänligen logga in</h1>
-  <form class="login" name='form' onsubmit="return log_val();" action="loginprocess.php" method='POST'>
+  <h1>Please enter your email and password</h1>
+  <form class="login" name='form' onsubmit="return log_val();" action="login-process.php" method='POST'>
     <label for="email">Email:  </label><br>
     <input type="text" name="email" value=""><br>
-    <label for="password">Lösenord: </label><br>
+    <label for="password">Password: </label><br>
     <input type="password" name="password" value=""><br><br>
-    <input type="submit" name="login" value="Logga in">
+    <input type="submit" name="login" value="Login">
   </div>
-  <p>Inte registrerad? <a href=register.php>Tryck här!</a></p>
+  <p>Not registered? <a href=register.php>Register</a></p>
   </form>
 </html>
